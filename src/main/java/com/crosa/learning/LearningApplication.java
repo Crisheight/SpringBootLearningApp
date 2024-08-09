f3997827c7f6c27a6142fdea5e986cbf26aa66d6
Cris <crisheight@gmail.com>
2024-08-09 17:22:02 -0500

    init

---
+++ src/main/java/com/crosa/learning/LearningApplication.java
@@ -0,0 +1,22 @@
+package com.crosa.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class LearningApplication {
+
+	public static void main(String[] args) {
+		var ctx = SpringApplication.run(LearningApplication.class, args);
+
+		FirstClass myFirstClass = ctx.getBean(FirstClass.class);
+		System.out.println(myFirstClass.sayHello());
+	}
+
+	@Bean
+	public FirstClass myfirstClass() {
+		return new FirstClass();
+	}
+
+}
