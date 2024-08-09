b9e51d828311e5f338f0786fff3fb32f42abf92f
Cris <crisheight@gmail.com>
2024-08-09 17:34:06 -0500

    Remove component annotation to split the configuration.

--- src/main/java/com/crosa/learning/FirstClass.java
+++ src/main/java/com/crosa/learning/FirstClass.java
@@ -1,10 +1,10 @@
 package com.crosa.learning;
 
-import org.springframework.stereotype.Component;
 
-@Component
 public class FirstClass {
+
     public String sayHello() {
         return "Hello from the FirstClass :) \n";
     }
+
 }
