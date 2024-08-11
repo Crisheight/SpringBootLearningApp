bc104923a2113fa4678e4306832d392548ae1ddf
Cris <crisheight@gmail.com>
2024-08-11 17:26:32 -0500

    Create a constructor for class object so we can use Autowired and inject the FirstClass bean into the FirstService class.

--- src/main/java/com/crosa/learning/FirstService.java
+++ src/main/java/com/crosa/learning/FirstService.java
@@ -1,7 +1,19 @@
 package com.crosa.learning;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FirstService {
+
+    private final FirstClass firstClass;
+
+    @Autowired
+    public FirstService(FirstClass firstClass) {
+        this.firstClass = firstClass;
+    }
+
+    public String tellAStory() {
+        return "the dependency is injected and says: " + firstClass.sayHello();
+    }
 }
