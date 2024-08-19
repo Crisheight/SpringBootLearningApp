53c9ee136e7bed376bdad19d80923f1b7fc9c7d8
Cris <crisheight@gmail.com>
2024-08-19 17:18:50 -0500

    Changed method name, created second Bean, added Qualifiers.

--- src/main/java/com/crosa/learning/ApplicationConfig.java
+++ src/main/java/com/crosa/learning/ApplicationConfig.java
@@ -1,14 +1,22 @@
 package com.crosa.learning;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfig {
 
-    @Bean("firstClassBean")
-    public FirstClass myfirstClass() {
+    @Bean
+    @Qualifier("bean1")
+    public FirstClass myFirstBean() {
         return new FirstClass("First bean :)");
+    }
+
+    @Bean
+    @Qualifier("bean2")
+    public FirstClass mySecondClass() {
+        return new FirstClass("Second bean :)");
     }
 
 }
