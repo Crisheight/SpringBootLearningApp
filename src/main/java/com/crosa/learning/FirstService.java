ac7f7961cac78c88a9fe236716037222724c7118
Crisheight <crisheight@gmail.com>
2024-12-13 20:44:17 -0600

    Create an environment bean with a setter that returns the OS name.

--- src/main/java/com/crosa/learning/FirstService.java
+++ src/main/java/com/crosa/learning/FirstService.java
@@ -20,8 +20,8 @@ public class FirstService {
         return "the dependency is injected and says: " + firstClass.sayHello();
     }
 
-    public String getJavaVersion() {
-        return environment.getProperty("java.version");
+    public String getOsName() {
+        return environment.getProperty("os.name");
     }
 
     @Autowired
