940256541230b6f2f4b03937b34bfd28fef91ccb
Crisheight <crisheight@gmail.com>
2024-12-18 15:07:01 -0600

    Set specific profiles for beans using @Profile. Create bean names for use.

--- src/main/java/com/crosa/learning/ApplicationConfig.java
+++ src/main/java/com/crosa/learning/ApplicationConfig.java
@@ -2,16 +2,19 @@ package com.crosa.learning;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class ApplicationConfig {
 
-    @Bean
+    @Bean("bean1")
+    @Profile("dev")
     public FirstClass myFirstBean() {
         return new FirstClass("First bean :)");
     }
 
-    @Bean
+    @Bean("bean2")
+    @Profile("test")
     public FirstClass mySecondBean() {
         return new FirstClass("Second bean :)");
     }
