da8fb5bf018e053c9b0b2f6eb766f9ab7614780a
Cris <crisheight@gmail.com>
2024-12-23 23:14:20 -0600

    Add a new POST request. Remove a GET request method.

--- src/main/java/com/crosa/learning/FirstController.java
+++ src/main/java/com/crosa/learning/FirstController.java
@@ -1,8 +1,7 @@
 package com.crosa.learning;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,13 +9,11 @@ public class FirstController {
 
     @GetMapping("/greetings")
     public String sayGreeting() {
-        return "Hello from the REST controller";
+        return "Hello from the GET request";
     }
 
-    @GetMapping("/greetings-2")
-    @ResponseStatus(HttpStatus.ACCEPTED)
-    public String sayGreeting2() {
-        return "Hello from the REST controller";
+    @PostMapping("/post")
+    public String post(String message){
+        return "Successful POST request. The message is: " + message;
     }
-
-}
+} // End FirstController
