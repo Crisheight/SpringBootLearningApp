55dff3b9c1b949800e01dda2a129d2c4d8410c71
Cris <crisheight@gmail.com>
2025-01-27 16:06:29 -0600

    created new mapping to pass request parameters

--- src/main/java/com/crosa/learning/FirstController.java
+++ src/main/java/com/crosa/learning/FirstController.java
@@ -25,11 +25,21 @@ public class FirstController {
         return "Request successful. The order is: " + order.toString();
     }
 
+    // http://localhost:8080/greetings/{user-name}
     @GetMapping("/greetings/{user-name}")
     public String pathVar(
             @PathVariable("user-name") String userName
     ) {
         return "my value = " + userName;
+    }
+
+    // http://localhost:8080/discover?param_name=paramvalue&param_name_2=value_2
+    @GetMapping("/discover")
+    public String paramVar(
+            @RequestParam("user-name") String userName,
+            @RequestParam("user-last-name") String userLastName
+    ) {
+        return "my value = " + userName + " " + userLastName;
     }
 
 } // End FirstController
