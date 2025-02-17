d54da640ef9228aef60d87c476a72babd363895e
Cris <crisheight@gmail.com>
2025-02-17 15:59:13 -0600

    End p1. Start p2 clean: delete Order class and OrderRecord class. Modify FirstController class - only contain initial get and post mapping.

--- src/main/java/com/crosa/learning/FirstController.java
+++ src/main/java/com/crosa/learning/FirstController.java
@@ -15,31 +15,4 @@ public class FirstController {
         return "Successful POST request. The message is: " + message;
     }
 
-    @PostMapping("/post-order")
-    public String post(@RequestBody Order order){
-        return "Request successful. The order is: " + order.toString();
-    }
-
-    @PostMapping("/post-order-record")
-    public String postRecord(@RequestBody OrderRecord order){
-        return "Request successful. The order is: " + order.toString();
-    }
-
-    // http://localhost:8080/greetings/{user-name}
-    @GetMapping("/greetings/{user-name}")
-    public String pathVar(
-            @PathVariable("user-name") String userName
-    ) {
-        return "my value = " + userName;
-    }
-
-    // http://localhost:8080/discover?param_name=paramvalue&param_name_2=value_2
-    @GetMapping("/discover")
-    public String paramVar(
-            @RequestParam("user-name") String userName,
-            @RequestParam("user-last-name") String userLastName
-    ) {
-        return "my value = " + userName + " " + userLastName;
-    }
-
 } // End FirstController
