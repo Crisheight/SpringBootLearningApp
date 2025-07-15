e8d64688b68472d8905bc867bf1f21143d95da8a
Cris <crisheight@gmail.com>
2025-07-15 17:30:08 -0400

    Implement new GetMapping(s) to return all students or student by id via GET requests.

--- src/main/java/com/crosa/learning/FirstController.java
+++ src/main/java/com/crosa/learning/FirstController.java
@@ -2,6 +2,8 @@ package com.crosa.learning;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class FirstController {
     private final StudentRepository studentRepository;
@@ -13,5 +15,18 @@ public class FirstController {
     @PostMapping("/students")
     public Student post(@RequestBody Student student) {
         return studentRepository.save(student);
+    }
+
+    @GetMapping("/students")
+    public List<Student> findAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @GetMapping("/students/{student-id}")
+    public Student findStudentById(
+            @PathVariable("student-id") Integer id
+    ) {
+        return studentRepository.findById(id)
+                .orElse(new Student());
     }
 } // End FirstController
