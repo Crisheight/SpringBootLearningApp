a80e1da6a33444b6e4642be9487d14db80942083
Cris <crisheight@gmail.com>
2025-07-17 18:23:08 -0400

    Renamed to StudentController from FirstController.

--- src/main/java/com/crosa/learning/StudentController.java
+++ src/main/java/com/crosa/learning/StudentController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-public class FirstController {
+public class StudentController {
     private final StudentRepository studentRepository;
 
-    public FirstController(StudentRepository studentRepository) {
+    public StudentController(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
 
