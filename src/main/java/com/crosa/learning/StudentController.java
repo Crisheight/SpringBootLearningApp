cf8d8c1fd5d964b167d3f8f6a5306c4f364e845a
Cris <crisheight@gmail.com>
2025-07-24 18:18:48 -0400

    Refactor to use the new service layer.

--- src/main/java/com/crosa/learning/StudentController.java
+++ src/main/java/com/crosa/learning/StudentController.java
@@ -4,59 +4,44 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class StudentController {
-    private final StudentRepository studentRepository;
 
-    private Student toStudent(StudentDto dto) {
-        var student = new Student();
+    private final StudentService studentService;
 
-        student.setFirstName(dto.firstName());
-        student.setLastName(dto.lastName());
-        student.setEmail(dto.email());
-
-        var school = new School();
-        school.setId(dto.schoolId());
-
-        student.setSchool(school);
-
-        return student;
-    }
-
-    private StudentResponseDTO toStudentResponseDTO(Student student) {
-        return new StudentResponseDTO(
-                student.getFirstName(),
-                student.getLastName(),
-                student.getEmail()
-        );
-    }
-
-    public StudentController(StudentRepository studentRepository
-    ) {
-        this.studentRepository = studentRepository;
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
     }
 
     @PostMapping("/students")
-    public StudentResponseDTO post(@RequestBody StudentDto dto
+    public StudentResponseDto saveStudent(@RequestBody StudentDto dto
     ) {
-        var student = toStudent(dto);
-        var savedStudent = studentRepository.save(student);
-
-        return toStudentResponseDTO(savedStudent);
+        return this.studentService.saveStudent(dto);
     }
 
     @GetMapping("/students")
     public List<Student> findAllStudents() {
-        return studentRepository.findAll();
+        return studentService.findAllStudents();
     }
 
     @GetMapping("/students/{student-id}")
     public Student findStudentById(
             @PathVariable("student-id") Integer id
     ) {
-        return studentRepository.findById(id)
-                .orElse(new Student());
+        return studentService.findStudentById(id);
+    }
+
+    @GetMapping("/students/search/{student-name")
+    public List<Student> findStudentByName(
+            @PathVariable("student-name") String name
+    ) {
+        return studentService.findAllStudents()
+                .stream()
+                .filter(student -> student.getFirstName().equalsIgnoreCase(name) ||
+                                   student.getLastName().equalsIgnoreCase(name))
+                .toList();
     }
 
     @DeleteMapping("/students/{student-id}")
@@ -64,6 +49,6 @@ public class StudentController {
     public void deleteStudentById(
             @PathVariable("student-id") Integer id
     ) {
-        studentRepository.deleteById(id);
+        studentService.deleteStudentById(id);
     }
 } // End FirstController
