b4198ea619efd9eec5b896bd3aba5afde4749fb8
Cris <crisheight@gmail.com>
2025-07-27 15:12:06 -0400

    Updated Repository to support StudentService method

--- src/main/java/com/crosa/learning/StudentRepository.java
+++ src/main/java/com/crosa/learning/StudentRepository.java
@@ -2,6 +2,9 @@ package com.crosa.learning;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StudentRepository extends JpaRepository<Student, Integer> {
 
+    List<Student> findAllByFirstNameContaining(String firstName);
 }
