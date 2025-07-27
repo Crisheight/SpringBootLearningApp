591e3789f41e8a9d70a1df79463dcbb2b26b5439
Cris <crisheight@gmail.com>
2025-07-27 15:39:05 -0400

    Begin implementing SchoolService to move extra logic out of SchoolController.

--- src/main/java/com/crosa/learning/SchoolService.java
+++ src/main/java/com/crosa/learning/SchoolService.java
@@ -1,4 +1,33 @@
 package com.crosa.learning;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class SchoolService {
+    private final SchoolRepository schoolRepository;
+    private final SchoolMapper schoolMapper;
+
+    public SchoolService(SchoolRepository schoolRepository, SchoolMapper schoolMapper) {
+        this.schoolRepository = schoolRepository;
+        this.schoolMapper = schoolMapper;
+    }
+
+    public SchoolDto create(SchoolDto dto) {
+        var school = schoolMapper.toSchool(dto);
+        return schoolRepository.save(school);
+    }
+
+    public List<SchoolDto> findAll() {
+        return schoolRepository.findAll()
+                .stream()
+                .map(school -> new SchoolDto(school.getName()))
+                .collect(Collectors.toList());
+    }
+
+    public void deleteById(Integer id) {
+        schoolRepository.deleteById(id);
+    }
 }
