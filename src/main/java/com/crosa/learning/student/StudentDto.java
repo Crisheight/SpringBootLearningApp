be65c72019387cae9ae79f08e2396932bfebc435
Cris <crisheight@gmail.com>
2025-07-28 18:36:55 -0400

    Add Spring Annotations for NotEmpty fields in the StudentDto object

--- src/main/java/com/crosa/learning/student/StudentDto.java
+++ src/main/java/com/crosa/learning/student/StudentDto.java
@@ -1,8 +1,14 @@
 package com.crosa.learning.student;
 
+import jakarta.validation.constraints.NotEmpty;
+
 public record StudentDto(
+        @NotEmpty
         String firstName,
+
+        @NotEmpty
         String lastName,
+
         String email,
         Integer schoolId
 ) {
