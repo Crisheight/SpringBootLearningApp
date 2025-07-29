c7f60852b2a4a236a77d241fa93ac076262aa34d
Cris <crisheight@gmail.com>
2025-07-29 15:16:47 -0400

    Implement test for the toStudent method from StudentMapper

--- src/test/java/com/crosa/learning/student/StudentMapperTest.java
+++ src/test/java/com/crosa/learning/student/StudentMapperTest.java
@@ -1,7 +1,37 @@
 package com.crosa.learning.student;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class StudentMapperTest {
 
-}
+    private StudentMapper studentMapper;
+
+    @BeforeEach
+    void setUp() {
+        studentMapper = new StudentMapper();
+    }
+
+    @Test
+    public void mapStudentDtoToStudent() {
+        StudentDto dto = new StudentDto(
+                "John",
+                "Doe",
+                "john@mail.com",
+                1
+        );
+        Student student = studentMapper.toStudent(dto);
+
+        assertEquals(dto.firstName(), student.getFirstName());
+        assertEquals(dto.lastName(), student.getLastName());
+        assertEquals(dto.email(), student.getEmail());
+
+        assertNotNull(student.getSchool());
+        assertEquals(dto.schoolId(), student.getSchool().getId());
+
+    }
+} // End StudentMapperTest
+
