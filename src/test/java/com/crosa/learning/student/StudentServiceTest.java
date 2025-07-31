0feda5b46d91bbe64c73c004ad4398db45a48e75
Cris <crisheight@gmail.com>
2025-07-30 20:40:30 -0400

    Implement testing based on StudentService class.

--- src/test/java/com/crosa/learning/student/StudentServiceTest.java
+++ src/test/java/com/crosa/learning/student/StudentServiceTest.java
@@ -1,7 +1,73 @@
 package com.crosa.learning.student;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class StudentServiceTest {
 
-}
+    @InjectMocks
+    private StudentService studentService;
+
+    @Mock
+    private StudentMapper studentMapper;
+    @Mock
+    private StudentRepository studentRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void saveStudent_shouldReturnSavedStudentResponseDto() {
+        StudentDto dto = new StudentDto(
+                "John",
+                "Doe",
+                "jd@mail.com",
+                1
+        );
+
+        Student student = new Student(
+                "John",
+                "Doe",
+                "jd@mail.com",
+                20
+        );
+
+        Student savedStudent = new Student(
+                "John",
+                "Doe",
+                "jd@mail.com",
+                20
+        );
+
+        savedStudent.setId(1);
+
+        when(studentMapper.toStudent(dto))
+                .thenReturn(student);
+
+        when(studentRepository.save(student))
+                .thenReturn(savedStudent);
+
+        when(studentMapper.toStudentResponseDto(savedStudent))
+                .thenReturn(new StudentResponseDto(
+                        "John",
+                        "Doe",
+                        "jd@mail.com")
+                );
+
+        StudentResponseDto studentResponseDto = studentService.saveStudent(dto);
+
+        assertEquals(dto.firstName(), studentResponseDto.firstName());
+        assertEquals(dto.lastName(), studentResponseDto.lastName());
+        assertEquals(dto.email(), studentResponseDto.email());
+
+    }
+} // End StudentServiceTest
